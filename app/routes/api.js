ab09b9a649519b2420c8a097e94ac5133b1b27cd
ryan-berkowitz <berkowitz.dev@gmail.com>
2018-06-09 21:44:55 -0400

    Added method for gingular GET of User.

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,7 +1,14 @@
 const models = require('./../../models')
 module.exports = function (server) {
+    // Read all users
     server.get('/api/users', function (req, res){
         models.User.findAll({}).then(function (result){
+            res.json(result)
+        })
+    })
+    // Read user by ID
+    server.get('/api/users/:id', function (req, res){
+        models.User.findById(req.params.id).then(function (result){
             res.json(result)
         })
     })
